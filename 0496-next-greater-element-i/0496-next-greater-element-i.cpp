f4d8f224366707b3722c79ce4e8d10e6bb677207
Krithika <krithikapalani08@gmail.com>
2026-06-12 13:21:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (61.08%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>s;
+        vector<int>res(nums1.size());
+        unordered_map<int,int>m;
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+            while(!s.empty() && s.top()<=nums2[i])
+              s.pop();
+            if(s.empty())
+              m[nums2[i]]=-1;
+            else
+              m[nums2[i]]=s.top();
+            s.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++)
+            res[i]=m[nums1[i]];
+        return res;
+    }
+};
