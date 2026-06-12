24191c85593111e0f69454fc2475e3964f470124
Krithika <krithikapalani08@gmail.com>
2026-06-12 17:32:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.94%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int a=x^y;
+        int count=0;
+        while(a)
+        {
+            a=a&(a-1);
+            count++;
+        }
+        return count;
+    }
+};
